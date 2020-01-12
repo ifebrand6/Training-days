1520daa516f5529f27b97d7b63839b1deb899f32
udeogu ifechuwku <ifebrand6@gmail.com>
2020-01-12 22:33:56 +0100

    made major changes to convertion fcn

--- Vowel_Interpreter.js
+++ Vowel_Interpreter.js
@@ -1,5 +1,29 @@
-let input = "win please win";
+var input = "turpentine and turtles";
 const vowels = ['a','e','i','o','u'];
 let resultArray = []
-for (i = 0; i < input.length; i++){
-}
+for (let i = 0; i < input.length; i++)
+for (let k = 0; k < vowels.length; k++)
+{
+    if (input[i] === vowels[k]){
+    resultArray.push(input[i]);
+        if (input[i] === 'e'){
+        resultArray.push(input[i] + 'e')
+     }; 
+        if (input[i] === 'u'){
+        resultArray.push(input[i] + 'u')
+     };
+    }
+};
+console.log(resultArray.join('').toUpperCase());
+
+
+// let input = "is";
+// const vowels = ['a','e','i','o','u'];
+// let resultArray = []
+// for (let i = 0; i < input.length; i++)
+// for (let k = 0; k < vowels.length; k++){
+//     if (input[i] === vowels[k]){
+//     resultArray.push(input[i])
+//     console.log(resultArray.length);
+//     }
+// }
