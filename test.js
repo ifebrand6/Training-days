577e702791854db99aaae351e7f99759140e1b8e
udeogu ifechuwku <ifebrand6@gmail.com>
2020-01-16 11:06:09 +0100

    creatd an ES5 version of code for browser comp...

--- test.js
+++ test.js
@@ -7,3 +7,31 @@ i++;
 } while (i < 5);
 
 console.log(countString);
+
+const delayed = () => { i + 2
+
+};
+
+const delay = function delay() {
+    console.log(`this anwser is ${i}`);
+};
+delay();
+
+function big(input) {
+    input / 2
+}
+let bigOj = {
+    okay: [],
+    peopel: function big(input) {
+        if (input > 0){
+            return input / 2
+        }
+        else {
+            console.log("bad input");
+        }
+
+    }
+    
+};
+let test = bigOj.peopel(0)
+console.log(test);
